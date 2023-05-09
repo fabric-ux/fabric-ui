f848b8d13f553d9435db993234e5f77b00555990
m.thariq <m.thariq@majoo.id>
2023-05-09 12:56:32 +0700

    feature: input component

--- packages/fabrix-core/src/Button/index.tsx
+++ packages/fabrix-core/src/Button/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import './button.css';
+
+export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export interface ButtonProps {
   children: React.ReactNode;
   /**
    * Specify button type
    */
   type: 'primary' | 'secondary' | 'ghost' | 'error' | 'negative';
-  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  size: Size
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ type, size, children, ...props }, ref) => {
--- packages/fabrix-core/src/index.tsx
+++ packages/fabrix-core/src/index.tsx
@@ -3,3 +3,4 @@ import './styles.css';
 
 export { Button, type ButtonProps } from "./Button"
 export { default as Flex, type FProps } from './Flex';
+export { default as Input, type InputProps } from './Input';
